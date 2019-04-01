8c1db758e572837011c9dbc550436cc420884a4f
Sieun Bae <qotldms130@gmail.com>
2019-04-01 23:37:01 +0900

    Create video game.java

---
+++ video game.java
@@ -0,0 +1,12 @@
+boolean canSeePlayer;
+boolean playerPoweredUp;
+
+if(canSeePlayer){
+  if(!playerPoweredUp) {
+    System.out.println(Attack!");
+   }  else {
+    System.out.println("Run away!");
+   }
+ }  else {
+    System.out.println("Wander.");
+ }
