024b0463d058f9195067d600b571033f45f52ec6
Sieun Bae <qotldms130@gmail.com>
2019-04-01 23:40:41 +0900

    Create weather.java

---
+++ weather.java
@@ -0,0 +1,9 @@
+boolean isSnowing = false;
+boolean isRaining = true;
+double temperature = 60.0;
+
+if(isSnowing || isRaining || temperature < 50) {
+  System.out.println("Let's stay home.");
+} else {
+  System.out.println("Let's go out!");
+}
