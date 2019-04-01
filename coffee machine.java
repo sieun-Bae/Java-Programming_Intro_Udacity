a159ececd0d869f99d6043eb51109383944bd5cd
Sieun Bae <qotldms130@gmail.com>
2019-04-01 23:31:59 +0900

    Create coffee machine.java
    switch-statement

---
+++ coffee machine.java
@@ -0,0 +1,20 @@
+int passcode;
+String coffeeType;
+
+switch(passcode) {
+  case 555: coffeeType = "Espresso";
+    break;
+  case 312: coffeeType = "Vanilla latte";
+    break;
+  case 629: coffeeType = "Unknown";
+    break;
+   default: coffeeType = "Invalid code";
+    break;
+}
+System.out.println(coffeeType);
+
+'''
+if (passcode == 555) {
+  coffeeType = "Espresso";
+} else if(){} else()
+'''
