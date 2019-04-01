d51fe02ad3c161f2f820ea7fd257a6dc16ddc24d
Sieun Bae <qotldms130@gmail.com>
2019-04-01 23:08:23 +0900

    Create self-driving car.java
    - {scope}

---
+++ self-driving car.java
@@ -0,0 +1,16 @@
+boolean isLightGreen = false;
+boolean isLightYellow = true;
+
+boolean isRaining;
+
+if(isLightGreen){
+  System.out.println("Drive!");
+} else if(isLightYellow){
+  System.out.println("Slow down.");
+} else{
+  System.out.println("Stop.");
+}
+
+if (isRaining){
+  System.out.println("Windshield wipers on.");
+}
